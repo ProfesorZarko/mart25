5226119116bdbc63f7270711f86b499aeb86248a
ProfesorZarko <profesorfz2@gmail.com>
2025-03-07 07:50:43 +0100

    Initial commit sisanje

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>drugi pokusaj za git hub</h2>
+      </div>
+      <div>
+        <h1>pozdrav maturanti</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
